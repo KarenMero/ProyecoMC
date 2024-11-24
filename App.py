abf2da71d4bc16b4a74eea2ec8588185c53f9288
StarKilled <elecuavene15@gmail.com>
2024-11-24 17:55:29 -0500

    Cambios en el app.py

--- App.py
+++ App.py
@@ -1,59 +1,138 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_mysqldb import MySQL
 from wsgiref.simple_server import make_server
-app=Flask(__name__)
-app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='flaskcontact'
-mysql=MySQL(app)
-app.secret_key ='mysecretkey'
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''  # Cambia si es necesario
+app.config['MYSQL_DB'] = 'TiendaMC'
+mysql = MySQL(app)
+
+app.secret_key = 'mysecretkey'
+
+
 @app.route('/')
-def Index():
-   cur= mysql.connection.cursor()
-   cur.execute('select * from Contacts')
-   data=cur.fetchall()
-   cur.close()
-   return render_template('index.html',contacts=data)
-@app.route('/add_contact', methods=['POST'])
-def add_contact():
-   if request.method=='POST':
-      fullname =request.form['fullname']
-      phone =request.form['phone']
-      email =request.form['email']
-      cur= mysql.connection.cursor()
-      cur.execute('INSERT INTO contacts (fullname,phone,email) VALUES (%s, %s ,%s)',(fullname,phone,email))
-      mysql.connection.commit()
-      cur.close()
-      flash('contacto agregado satisfactoriamente')
-      return redirect(url_for('Index'))
-@app.route('/edit/<id>')
-def get_contact(id):
-   cur=mysql.connection.cursor()
-   cur.execute('select * from Contacts where id={0}'.format(id)  )
-   data=cur.fetchall()
-   print(data[0])
-   return render_template('edit-contact.html',contact=data[0])
-@app.route('/update/<id>', methods=['POST'] )
-def update_contact(id):
-   if request.method=='POST':
-      fullname=request.form['fullname']
-      phone=request.form['phone']
-      email=request.form['email']
-      cur=mysql.connection.cursor()
-      cur.execute('update contacts set fullname=%s, phone=%s, email=%s where id={0}'.format(id), (fullname,phone,email)) 
-      mysql.connection.commit()
-      print(id)
-      cur.close()
-      flash('datos actualizados satisfactoriamente')
-      return redirect(url_for('Index'))
-@app.route('/delete/<string:id>')
-def delete_contact(id):
-   cur=mysql.connection.cursor()
-   cur.execute('delete from contacts where id={0}'.format(id))
-   mysql.connection.commit()
-   cur.close()
-   flash('datos eliminados satisfactoriamente')
-   return redirect(url_for('Index'))
-if __name__=='__main__':
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM cliente')  # Cambiar a cliente para obtener los datos
+    data = cur.fetchall()
+    cur.close()
+    return render_template('index.html', clientes=data)
+
+
+@app.route('/add_cliente', methods=['POST'])
+def add_cliente():
+    if request.method == 'POST':
+        nombres = request.form['nombres']
+        apellidos = request.form['apellidos']
+        direccion = request.form['direccion']
+        telefono = request.form['telefono']
+        correoElectronico = request.form['correoElectronico']
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO cliente (nombres, apellidos, direccion, telefono, correoElectronico) VALUES (%s, %s, %s, %s, %s)', 
+                    (nombres, apellidos, direccion, telefono, correoElectronico))
+        mysql.connection.commit()
+        cur.close()
+        flash('Cliente agregado satisfactoriamente')
+        return redirect(url_for('index'))
+
+
+@app.route('/edit_cliente/<id>')
+def edit_cliente(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM cliente WHERE idCliente = {0}'.format(id))
+    data = cur.fetchall()
+    cur.close()
+    return render_template('edit_cliente.html', cliente=data[0])
+
+
+@app.route('/update_cliente/<id>', methods=['POST'])
+def update_cliente(id):
+    if request.method == 'POST':
+        nombres = request.form['nombres']
+        apellidos = request.form['apellidos']
+        direccion = request.form['direccion']
+        telefono = request.form['telefono']
+        correoElectronico = request.form['correoElectronico']
+        cur = mysql.connection.cursor()
+        cur.execute('UPDATE cliente SET nombres = %s, apellidos = %s, direccion = %s, telefono = %s, correoElectronico = %s WHERE idCliente = {0}'.format(id), 
+                    (nombres, apellidos, direccion, telefono, correoElectronico))
+        mysql.connection.commit()
+        cur.close()
+        flash('Cliente actualizado satisfactoriamente')
+        return redirect(url_for('index'))
+
+
+@app.route('/delete_cliente/<string:id>')
+def delete_cliente(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM cliente WHERE idCliente = {0}'.format(id))
+    mysql.connection.commit()
+    cur.close()
+    flash('Cliente eliminado satisfactoriamente')
+    return redirect(url_for('index'))
+
+
+@app.route('/productos')
+def productos():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM producto')  # Obtiene los productos
+    data = cur.fetchall()
+    cur.close()
+    return render_template('productos.html', productos=data)
+
+
+@app.route('/add_producto', methods=['POST'])
+def add_producto():
+    if request.method == 'POST':
+        nombreSerie = request.form['nombreSerie']
+        precio = request.form['precio']
+        stock = request.form['stock']
+        idProveedor = request.form['idProveedor']
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO producto (nombreSerie, precio, stock, idProveedor) VALUES (%s, %s, %s, %s)', 
+                    (nombreSerie, precio, stock, idProveedor))
+        mysql.connection.commit()
+        cur.close()
+        flash('Producto agregado satisfactoriamente')
+        return redirect(url_for('productos'))
+
+
+@app.route('/edit_producto/<id>')
+def edit_producto(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM producto WHERE idProducto = {0}'.format(id))
+    data = cur.fetchall()
+    cur.close()
+    return render_template('edit_producto.html', producto=data[0])
+
+
+@app.route('/update_producto/<id>', methods=['POST'])
+def update_producto(id):
+    if request.method == 'POST':
+        nombreSerie = request.form['nombreSerie']
+        precio = request.form['precio']
+        stock = request.form['stock']
+        idProveedor = request.form['idProveedor']
+        cur = mysql.connection.cursor()
+        cur.execute('UPDATE producto SET nombreSerie = %s, precio = %s, stock = %s, idProveedor = %s WHERE idProducto = {0}'.format(id), 
+                    (nombreSerie, precio, stock, idProveedor))
+        mysql.connection.commit()
+        cur.close()
+        flash('Producto actualizado satisfactoriamente')
+        return redirect(url_for('productos'))
+
+
+@app.route('/delete_producto/<string:id>')
+def delete_producto(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM producto WHERE idProducto = {0}'.format(id))
+    mysql.connection.commit()
+    cur.close()
+    flash('Producto eliminado satisfactoriamente')
+    return redirect(url_for('productos'))
+
+
+if __name__ == '__main__':
     app.run(port=3000, debug=True)
